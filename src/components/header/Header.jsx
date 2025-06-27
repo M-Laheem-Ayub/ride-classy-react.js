f044a6d6e96df603f9cf2bc6e49093bd7cf00a39
Laheem Ayub <tyou67906@gmail.com>
2025-06-28 04:47:47 +0500

    set header toggle

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -36,7 +36,6 @@ const Header = () => {
             <span className="sr-only">Open main menu</span>
             <svg
               className="w-5 h-5"
-              aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 17 14"
@@ -59,9 +58,16 @@ const Header = () => {
             id="navbar-default"
           >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+              
               {/* Admin */}
-              <li className="flex items-center -ms-2 md:-ms-0  order-1 md:order-4">
-                <NavLink to="/admin" className="flex items-center space-x-2 px-4 py-1">
+              <li
+                className="flex items-center -ms-2 md:-ms-0 order-1 md:order-4"
+                onClick={() => setMenuOpen(false)}
+              >
+                <NavLink
+                  to="/admin"
+                  className="flex items-center space-x-2 px-4 py-1"
+                >
                   <img
                     src="https://i.pravatar.cc/40?img=12"
                     alt="Admin Profile"
@@ -72,11 +78,14 @@ const Header = () => {
               </li>
 
               {/* Home */}
-              <li className="order-2 md:order-1">
+              <li
+                className="order-2 md:order-1"
+                onClick={() => setMenuOpen(false)}
+              >
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
-                    `flex items-center  gap-3.5 md:gap-2  py-2 px-3 rounded ${
+                    `flex items-center gap-3.5 md:gap-2 py-2 px-3 rounded ${
                       isActive
                         ? "text-black font-semibold"
                         : "text-gray-500 hover:text-black"
@@ -92,11 +101,14 @@ const Header = () => {
               </li>
 
               {/* Contact */}
-              <li className="order-3 md:order-2">
+              <li
+                className="order-3 md:order-2"
+                onClick={() => setMenuOpen(false)}
+              >
                 <NavLink
                   to="/contact"
                   className={({ isActive }) =>
-                    `flex items-center  gap-3.5 md:gap-2 py-2 px-3 rounded ${
+                    `flex items-center gap-3.5 md:gap-2 py-2 px-3 rounded ${
                       isActive
                         ? "text-black font-semibold"
                         : "text-gray-500 hover:text-black"
@@ -112,7 +124,10 @@ const Header = () => {
               </li>
 
               {/* Book Online */}
-              <li className="order-4 md:order-3">
+              <li
+                className="order-4 md:order-3"
+                onClick={() => setMenuOpen(false)}
+              >
                 <NavLink
                   to="/book-online"
                   className={({ isActive }) =>
